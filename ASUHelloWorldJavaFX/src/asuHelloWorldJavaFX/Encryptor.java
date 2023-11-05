c56ce8ceb5c322dd0d899abd286f1610768c2605
shahih25 <hmshahid@asu.edu>
2023-11-05 13:25:38 -0700

    Add security questions

--- ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/Encryptor.java
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/Encryptor.java
@@ -1,38 +1,48 @@
 package asuHelloWorldJavaFX;
 
 public class Encryptor {
+	
+	private static int OFFSET = 15;
 
-    private static int OFFSET = 15;
+	public static String encrypt(String input)
+	{
+		String output = "";
+		for(int i = 0; i < input.length(); i++)
+		{
+			if(input.charAt(i) == '\n') { output += '\n'; }
+			else
+			{
+				if(i % 2 == 0)
+				{
+					output += (char) (input.charAt(i) + OFFSET);
+				}
+				else
+				{
+					output += (char) (input.charAt(i) + OFFSET);
+				}
+			}
+		}
+		return output;
+	}
 
-    public static String encrypt(String input) {
-        String output = "";
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '\n') {
-                output += '\n';
-            } else {
-                if (i % 2 == 0) {
-                    output += (char) (input.charAt(i) + OFFSET);
-                } else {
-                    output += (char) (input.charAt(i) + OFFSET);
-                }
-            }
-        }
-        return output;
-    }
-
-    public static String decrypt(String input) {
-        String output = "";
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '\n') {
-                output += '\n';
-            } else {
-                if (i % 2 == 0) {
-                    output += (char) (input.charAt(i) - OFFSET);
-                } else {
-                    output += (char) (input.charAt(i) - OFFSET);
-                }
-            }
-        }
-        return output;
-    }
+	public static String decrypt(String input)
+	{
+		String output = "";
+		for(int i = 0; i < input.length(); i++)
+		{
+			if(input.charAt(i) == '\n') { output += '\n'; }
+			else
+			{
+				if(i % 2 == 0)
+				{
+					output += (char) (input.charAt(i) - OFFSET);
+				}
+				else
+				{
+					output += (char) (input.charAt(i) - OFFSET);
+				}
+			}
+		}
+		return output;
+	}
 }
--- ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ReadWrite.java
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ReadWrite.java
@@ -6,57 +6,61 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public final class ReadWrite {
+	
+	private String path;
+	public ReadWrite(String path)
+	{
+		this.path = path;
+	}
 
-    private String path;
+	public void write(String input)
+	{
+		try
+		{
+			FileWriter writer = new FileWriter(path);
+		    writer.write(Encryptor.encrypt(input));
+		    writer.close();
+		    System.out.println("Success");
+		} catch (IOException e) {
+			System.out.println("ERROR IN WRITING TO FILE");
+			e.printStackTrace();
+		}
+	}
 
-    public ReadWrite(String path) {
-        this.path = path;
-    }
+	public void append(String input, boolean newLine)
+	{
+		try
+		{
+			FileWriter writer = new FileWriter(path, true);
+			if(newLine) { input = "\n" + input; }
+		    writer.write(Encryptor.encrypt(input));
+		    writer.close();
+		} catch (IOException e) {
+			System.out.println("ERROR IN WRITING TO FILE");
+			e.printStackTrace();
+		}
+	}
 
-    public void write(String input) {
-        try {
-            FileWriter writer = new FileWriter(path);
-            writer.write(Encryptor.encrypt(input));
-            writer.close();
-            System.out.println("Success");
-        } catch (IOException e) {
-            System.out.println("ERROR IN WRITING TO FILE");
-            e.printStackTrace();
-        }
-    }
-
-    public void append(String input, boolean newLine) {
-        try {
-            FileWriter writer = new FileWriter(path, true);
-            if (newLine) {
-                input = "\n" + input;
-            }
-            writer.write(Encryptor.encrypt(input));
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("ERROR IN WRITING TO FILE");
-            e.printStackTrace();
-        }
-    }
-
-    public String read() {
-        try {
-            Scanner scan = new Scanner(new File(path));
-            String output = "";
-
-            while (scan.hasNextLine()) {
-                output += scan.nextLine() + "\n";
-            }
-
-            scan.close();
-
-            output = Encryptor.decrypt(output);
-
-            return output.substring(0, output.length() - 1);
-
-        } catch (Exception e) {
-            System.out.println("ERROR IN READING FILE");
-            return null;
-        }
-    }
+	public String read()
+	{
+		try {
+			Scanner scan = new Scanner(new File(path));
+			String output = "";
+			
+			while(scan.hasNextLine())
+			{
+				output += scan.nextLine() + "\n";
+			}
+			
+			scan.close();
+			
+			output = Encryptor.decrypt(output);
+			
+			return output.substring(0, output.length() - 1);
+			
+		} catch (Exception e) {
+			System.out.println("ERROR IN READING FILE");
+			return null;
+		}
+	}
 }
