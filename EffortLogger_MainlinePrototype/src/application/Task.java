2a23f561c6aac8a306e132f92f4eb4b9e827c56c
Talonflame23185 <92192572+Talonflame23185@users.noreply.github.com>
2023-11-11 13:09:46 -0800

    Add files via upload
    contains historicaldata.java file which implementation historical project data into planning poker

---
+++ EffortLogger_MainlinePrototype/src/application/Task.java
@@ -0,0 +1,28 @@
+package application;
+
+public class Task {
+	private String task;
+	
+	private String estimates;
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public String getEstimates() {
+		return estimates;
+	}
+
+	public void setEstimates(String estimates) {
+		this.estimates = estimates;
+	}
+	
+	public Task(String task, String estimates) {
+		this.setTask(task);
+		this.setEstimates(estimates);
+	}
+}
