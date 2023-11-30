100512602b968d9c5a1da2ca15267df81bc40073
KingOneGuy <47288277+KingOneGuy@users.noreply.github.com>
2023-11-30 16:06:11 -0700

    Comments for ReadWrite.java

--- EffortLoggerV2/src/effortloggerv2/ReadWrite.java
+++ EffortLoggerV2/src/effortloggerv2/ReadWrite.java
@@ -5,14 +5,30 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+// @author Frans Emil Malapo
+// Class to control reading and writing files for data storage
+// Automatically writes files using encryption from Encryptor
+// Assumes files read are encrypted by Encryptor
 public final class ReadWrite {
 	
-	private String path;
+	private String path;  // Path of the text file to read and write to
+	
+	// Creates new ReadWrite object using specified path
+	// Suggested that the path given should already exist as a text file. Otherwise, errors may occur.
+	//  (Particularly the read() function)
+	// @param path - file path of the text document to write to
+	//                  Make sure to include the .txt extension within the string
+	//  Example: "src\\filename.txt" <- filename.txt is stored in the same folder as these classes
+	//                                    "\\" serves as an escape character to represent "\"
 	public ReadWrite(String path)
 	{
 		this.path = path;
 	}
-
+	
+	// Writes to the file given from the string path
+	// If a file already exists in the path, the contents will be replaced with the input
+	// Prints error if fails
+	// @param input - String input to be written to file at path
 	public void write(String input)
 	{
 		try
@@ -27,6 +43,11 @@ public final class ReadWrite {
 		}
 	}
 
+	// Appends to the file given from the string path.
+	// Second argument determines whether or not to append to a new line
+	// Prints error if fails
+	// @param input - String input to be appended to file at path
+	// @param newLine - whether or not to append input to a new line. True if yes, false if no.
 	public void append(String input, boolean newLine)
 	{
 		try
@@ -41,6 +62,9 @@ public final class ReadWrite {
 		}
 	}
 
+	// Returns the contents of the text file at path as a String
+	// Prints error if fails
+	// @return - String contents of file
 	public String read()
 	{
 		try {
