b43988edc2c65bd3b0bea5965b375ac621ecbbb6
KingOneGuy <47288277+KingOneGuy@users.noreply.github.com>
2023-11-30 16:11:35 -0700

    Comments for Encryptor.java

--- EffortLoggerV2/src/effortloggerv2/Encryptor.java
+++ EffortLoggerV2/src/effortloggerv2/Encryptor.java
@@ -1,9 +1,15 @@
 package effortloggerv2;
 
+// @author Frans Emil Malapo
+// Class to encrypt and decrypt Strings using an offset key
+// All functions are static and meant for use without instantiation
 public class Encryptor {
 	
-	private static int OFFSET = 15;
+	private static int OFFSET = 15; // Key for encryption
 
+	// Encrypts a given unencrypted string
+	// @param input - String to encrypt
+	// @return - encrypted String
 	public static String encrypt(String input)
 	{
 		String output = "";
@@ -25,6 +31,9 @@ public class Encryptor {
 		return output;
 	}
 
+	// Decrpyts a given encrypted string and returns decrypted String
+	// @param input - encrypted String to decrypt
+	// @return - decrypted String
 	public static String decrypt(String input)
 	{
 		String output = "";
