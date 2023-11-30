7fad2ee20cd8da77e7c381247981aad521ba61e6
KingOneGuy <47288277+KingOneGuy@users.noreply.github.com>
2023-11-30 16:18:02 -0700

    Comments for EffortLog.java

--- EffortLoggerV2/src/FXDirectoryExcel/EffortLog.java
+++ EffortLoggerV2/src/FXDirectoryExcel/EffortLog.java
@@ -1,6 +1,11 @@
 package FXDirectoryExcel;
 
+//  @author Frans Emil Malapo
+//  Class representing Effort Logs as a Java Object
+//  Implements getters and setters to read and modify the object
 public class EffortLog {
+
+	//  Each variable corresponds to its Effort Log category
 	private int number;
 	private String date;
 	private String start;
@@ -9,7 +14,9 @@ public class EffortLog {
 	private String lifeCycleStep;
 	private String category;
 	private String delInt;  // Deliverable / Interruption / etc.
-	
+
+	//  Creates a Effort Log with default values
+	//  Strings are set to null, and integers are set to -1
 	public EffortLog()
 	{
 		number = -1;
@@ -22,17 +29,15 @@ public class EffortLog {
 		delInt = null;
 	}
 	
-	/**
-	 * Effort log entry for EffortLoggerV2.
-	 * @param number		Sequential number assigned to each entry.
-	 * @param date			Date entry was registered.
-	 * @param start			Time task was started.
-	 * @param stop			Time task was stopped.
-	 * @param timeElapsed	Total time spent on task in minutes.
-	 * @param lifeCycleStep	Project life cycle stage this entry belongs to.
-	 * @param category		What kind of task this entry represents.
-	 * @param delInt		Name of the task/entry.
-	 */
+	//  Effort log entry for EffortLoggerV2.
+	//  @param number		Sequential number assigned to each entry.
+	//  @param date			Date entry was registered.
+	//  @param start		Time task was started.
+	//  @param stop			Time task was stopped.
+	//  @param timeElapsed	Total time spent on task in minutes.
+	//  @param lifeCycleStep	Project life cycle stage this entry belongs to.
+	//  @param category		What kind of task this entry represents.
+	//  @param delInt		Name of the task/entry.
 	public EffortLog(int number, String date, String start, String stop, double timeElapsed,
 						String lifeCycleStep, String category, String delInt)
 	{
@@ -46,62 +51,79 @@ public class EffortLog {
 		this.delInt = delInt;
 	}
 	
-	public int getNumber() {
-		return number;
-	}
-	public void setNumber(int number) {
-		this.number = number;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public String getStart() {
-		return start;
-	}
-	public void setStart(String start) {
-		this.start = start;
-	}
-	public String getStop() {
-		return stop;
-	}
-	public void setStop(String stop) {
-		this.stop = stop;
-	}
-	public double getTimeElapsed() {
-		return timeElapsed;
-	}
-	public void setTimeElapsed(double timeElapsed) {
-		this.timeElapsed = timeElapsed;
-	}
-	public String getLifeCycleStep() {
-		return lifeCycleStep;
-	}
-	public void setLifeCycleStep(String lifeCycleStep) {
-		this.lifeCycleStep = lifeCycleStep;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public String getDelInt() {
-		return delInt;
-	}
-	public void setDelInt(String delInt) {
-		this.delInt = delInt;
-	}
-	
-	/**
-	 * Returns a String representation of the effort entry with fields separated by tabs.
-	 */
+	//  Returns a String representation of the Defect Log
+	//  @return String representation of Defect Log
 	public String toString()
 	{
 		return number + "\t" + date + "\t" + start  + "\t" + stop + "\t" + timeElapsed +
 				"\t" + lifeCycleStep + "\t" + category + "\t" + delInt;
 	}
 	
+	// ========Getters========
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public String getStart() {
+		return start;
+	}
+	
+	public String getStop() {
+		return stop;
+	}
+	
+	public double getTimeElapsed() {
+		return timeElapsed;
+	}
+	
+	public String getLifeCycleStep() {
+		return lifeCycleStep;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public String getDelInt() {
+		return delInt;
+	}
+	
+	// ========Setters========
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public void setStart(String start) {
+		this.start = start;
+	}
+	
+	public void setStop(String stop) {
+		this.stop = stop;
+	}
+	
+	public void setTimeElapsed(double timeElapsed) {
+		this.timeElapsed = timeElapsed;
+	}
+	
+	public void setLifeCycleStep(String lifeCycleStep) {
+		this.lifeCycleStep = lifeCycleStep;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setDelInt(String delInt) {
+		this.delInt = delInt;
+	}
 }
